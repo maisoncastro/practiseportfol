47c2bb06e0e75cb057bcefd11642674fc1078af8
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2021-07-21 21:39:03 -0400

    Made some changes

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="container">
+                <div className="row v-center space-between">
+                    <div className="logo">
+                        <a href="/">AGENCY.</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default header
